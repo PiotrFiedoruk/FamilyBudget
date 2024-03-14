2f36576aeb9ed2d2c7261c4aa6be2b045c29264e
PiotrFiedoruk <fiedorukpiotr@gmail.com>
2024-03-14 23:48:44 +0100

    add restframework to INSTALLED_APPS

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'backend.apps.users',
     'backend.apps.budgets'
 ]
