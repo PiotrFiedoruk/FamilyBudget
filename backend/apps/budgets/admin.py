6cc6d10b74f067259d695f7ccf534b228979efd6
PiotrFiedoruk <fiedorukpiotr@gmail.com>
2024-03-14 23:13:14 +0100

    register admin models

--- backend/apps/budgets/admin.py
+++ backend/apps/budgets/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Budget, BudgetOperation
+
+
+# Register your models here.
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner')
+
+
+@admin.register(BudgetOperation)
+class BudgetOperationAdmin(admin.ModelAdmin):
+    list_display = ('category', 'amount', 'budget')
