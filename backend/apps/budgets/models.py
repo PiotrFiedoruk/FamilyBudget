7179ace9bd2c094f731cee367cd6f967959a5e51
PiotrFiedoruk <fiedorukpiotr@gmail.com>
2024-03-14 16:44:31 +0100

    add Budget model

--- backend/apps/budgets/models.py
+++ backend/apps/budgets/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Budget(models.Model):
+    """
+    Budget model wraps BudgetOperation objects and ties them to user.
+    """
+    name = models.CharField(max_length=256)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='budgets')
+    shared_with = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='shared_with', blank=True)
+
+    class Meta:
+        unique_together = ['name', 'owner']
+
+    def __str__(self):
+        return self.name
