9d37a7557fe27c4a94f8b252660353485d8f6ca6
PiotrFiedoruk <fiedorukpiotr@gmail.com>
2024-03-15 10:24:21 +0100

    add redirect '/' -> 'api/'

--- backend/urls.py
+++ backend/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+
 from backend.apps.budgets.urls import urlpatterns as budgets_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('', RedirectView.as_view(url='api/'), name='api')
 
 ]
 urlpatterns += budgets_urlpatterns
